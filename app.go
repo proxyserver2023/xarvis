466b99334df5898355052dd1aae9b060f8fe7f84
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-07 12:27:21 +0600

    app.go will be used as a gateway for main

---
+++ app.go
@@ -0,0 +1,36 @@
+package main
+
+/*
+
+import (
+	"fmt"
+	"go_rest_api/pkg"
+	"go_rest_api/pkg/config"
+	"go_rest_api/pkg/mongo"
+	"go_rest_api/pkg/server"
+	"log"
+)
+
+type App struct {
+	server  *server.Server
+	session *mongo.Session
+	config  *root.Config
+}
+
+func (a *App) Initialize() {
+	a.config = config.GetConfig()
+	var err error
+	a.session, err = mongo.NewSession(a.config.Mongo)
+	if err != nil {
+		log.Fatalln("unable to connect to mongodb")
+	}
+
+	u := mongo.NewUserService(a.session.Copy(), a.config.Mongo)
+	a.server = server.NewServer(u, a.config)
+}
+
+func (a *App) Run() {
+	fmt.Println("Run")
+	defer a.session.Close()
+	a.server.Start()
+} */
