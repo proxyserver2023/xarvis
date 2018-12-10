1b1e3bff13e0bbf02ec4436cfb2db3f5eda528ac
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-10 20:33:02 +0600

    pseudo code for app

--- v3/app/app.go
+++ v3/app/app.go
@@ -4,4 +4,11 @@ package app
 // main passes the control to this Method and it does the magic.
 func Run() {
 
+	// 1. load application configuration
+	// 2. load error messages incase config import failed
+	// 3. create the logger
+	// 4. connect to db
+	// 5. registering API routes
+	// 6. start the server
+
 }
