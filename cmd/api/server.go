576f5b7136030353466ef870112693bf830a3e87
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-19 22:12:13 +0600

    :alembic: boilerplate func

---
+++ cmd/api/server.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// load application configuration
+	// load error messages
+	// create the logger
+	// connect to the database
+	// wire up api routing
+	// start the server
+}
