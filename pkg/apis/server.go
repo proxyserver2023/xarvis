2c946d0d56fd1362784a5c041a533023a6ff80c1
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-20 09:30:08 +0600

    :alembic: api gateway for this app

--- pkg/apis/server.go
+++ pkg/apis/server.go
@@ -3,10 +3,30 @@ package apis
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/alamin-mahamud/gapi/pkg/apis/routers"
+	"github.com/alamin-mahamud/gapi/pkg/app"
+	"github.com/alamin-mahamud/gapi/pkg/daos"
+	"github.com/alamin-mahamud/gapi/pkg/utils"
 )
+
+func setDBConn() (daos.DB, error) {
+	m := daos.NewMySQL()
+	err := m.Open(dbConfig)
+	utils.CheckErr(err)
+	return m, err
+}
+
+var dbConfig = &daos.DBConfig{}
 
 // Run runs the entire app
 func Run() {
-	fmt.Println("Server Running")
-	http.ListenAndServe(":8081", nil)
+	app.LoadEnvVars(dbConfig)
+
+	DB, err := setDBConn()
+	utils.CheckErr(err)
+
+	r := routers.BuildRouter(DB)
+	fmt.Println("Server listening at 8081")
+	http.ListenAndServe(":8081", r)
 }
