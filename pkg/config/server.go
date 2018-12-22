e6963acc9914917ce28ca19b4560ced31c927a52
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-22 16:22:30 +0600

    :wrench: server related cofig

---
+++ pkg/config/server.go
@@ -0,0 +1,9 @@
+package config
+
+// Server holds data necessery for server configuration
+type Server struct {
+	Port         string `yaml:"port,omitempty"`
+	Debug        bool   `yaml:"debug,omitempty"`
+	ReadTimeout  int    `yaml:"read_timeout_seconds,omitempty"`
+	WriteTimeout int    `yaml:"write_timeout_seconds,omitempty"`
+}
