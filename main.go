11168cc7c47dcdf9649b655f50743258a138988e
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-26 00:41:18 +0600

    :beers: cors middleware

--- main.go
+++ main.go
@@ -1,49 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/rs/cors"
 
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
 )
 
-func dummy(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "weadskfjksadljfklajs")
-}
-
-func dummy2(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "13412341234134")
-}
-
-func Middleware1(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	fmt.Println("1")
-	next(w, r)
-	fmt.Println("6")
-}
-
-func Middleware2(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	fmt.Println("2")
-	next(w, r)
-	fmt.Println("5")
-}
-
-func Middleware3(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	fmt.Println("3")
-	next(w, r)
-	fmt.Println("4")
-}
-
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	apiRoutes := mux.NewRouter()
-	common := negroni.New(
-		negroni.HandlerFunc(Middleware1),
-		negroni.HandlerFunc(Middleware2),
+	router := initRouter()
+	handler := initMiddleware(router)
+	http.ListenAndServe(":8080", handler)
+}
+
+func initMiddleware(h http.Handler) http.Handler {
+	n := negroni.New()
+	c := cors.New(
+		cors.Options{
+			AllowedOrigins: []string{"http://foo.com"},
+		},
 	)
-	router.PathPrefix("/").Handler(common.With(
-		negroni.HandlerFunc(Middleware3),
-		negroni.Wrap(apiRoutes),
-	))
-	http.ListenAndServe(":8080", router)
+	n.Use(c)
+	n.UseHandler(h)
+	return n
+}
+
+func initRouter() http.Handler {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{\"hello\": \"world\"}"))
+	})
+	return router
 }
