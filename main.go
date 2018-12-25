a10638d7d9da443d33692e2ef49e39b3a7982dce
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-26 00:27:46 +0600

    :beers: middleware chaining

--- main.go
+++ main.go
@@ -16,30 +16,33 @@ func dummy2(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "13412341234134")
 }
 
-func Middleware1(next http.Handler) http.Handler {
-	// do some stuff
-	// call the next middleware
-	// do some stuff
-	return next
+func Middleware1(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	fmt.Println("1")
+	next(w, r)
+	fmt.Println("6")
 }
 
-func Middleware2(next http.Handler) http.Handler {
-	// do some stuff
-	// call the next middleware
-	// do some stuff
-	return next
+func Middleware2(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	fmt.Println("2")
+	next(w, r)
+	fmt.Println("5")
+}
+
+func Middleware3(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	fmt.Println("3")
+	next(w, r)
+	fmt.Println("4")
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	apiRoutes := mux.NewRouter()
-	webRoutes := mux.NewRouter()
 	common := negroni.New(
-		Middleware1,
-		Middleware2,
+		negroni.HandlerFunc(Middleware1),
+		negroni.HandlerFunc(Middleware2),
 	)
 	router.PathPrefix("/").Handler(common.With(
-		APIMiddleware1,
+		negroni.HandlerFunc(Middleware3),
 		negroni.Wrap(apiRoutes),
 	))
 	http.ListenAndServe(":8080", router)
