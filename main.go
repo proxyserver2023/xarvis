623b4ffc6a57cfbfffe591b6b50ff4b21d809056
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-25 22:00:19 +0600

    :beers: negroni with custom server

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/urfave/negroni"
 )
 
+func dummy(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "weadskfjksadljfklajs")
+}
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", dummy)
 	n := negroni.Classic()
-	n.Run(":8080")
+	n.UseHandler(mux)
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        n,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(s.ListenAndServe())
 }
