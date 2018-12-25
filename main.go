669454953374d1a8ba07ee614d066c0bc1874304
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-25 22:17:19 +0600

    :beers: route specific middleware

--- main.go
+++ main.go
@@ -5,15 +5,30 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
 func dummy(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "weadskfjksadljfklajs")
 }
 
+func dummy2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "13412341234134")
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	adminRoutes := router.PathPrefix("/admin").Subrouter()
 	adminRoutes.HandleFunc("/", dummy)
+	adminRoutes.HandleFunc("/{id}", dummy2)
+
+	router.PathPrefix("/admin").Handler(
+		negroni.New(
+			// Middleware - 1,
+			// Middleware - 2,
+			negroni.Wrap(adminRoutes),
+		),
+	)
+
 	http.ListenAndServe(":8080", router)
 }
