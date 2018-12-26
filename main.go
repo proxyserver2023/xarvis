c99b147d33afe8be98e998c6d7413b1aa924d004
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-26 09:30:22 +0600

    :sparkles: one line entrypoint

--- main.go
+++ main.go
@@ -1,58 +1,7 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/rs/cors"
-
-	"github.com/gorilla/mux"
-	"github.com/meatballhat/negroni-logrus"
-	"github.com/urfave/negroni"
-)
+import "github.com/alamin-mahamud/gapi/pkg/app"
 
 func main() {
-	router := initRouter()
-	handler := initMiddleware(router)
-	http.ListenAndServe(":8080", handler)
-}
-
-func initMiddleware(h http.Handler) http.Handler {
-	n := negroni.New()
-	initRecoveryMiddleware(n)
-	initCORSMiddleware(n)
-	initLoggerMiddleware(n)
-	n.UseHandler(h)
-	return n
-}
-
-func initRouter() http.Handler {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("{\"hello\": \"world\"}"))
-	})
-	return router
-}
-
-func initCORSMiddleware(n *negroni.Negroni) {
-	c := cors.New(
-		cors.Options{
-			AllowedOrigins: []string{"http://foo.com"},
-		},
-	)
-	n.Use(c)
-}
-
-func initLoggerMiddleware(n *negroni.Negroni) {
-	// l := negroni.NewLogger()
-	// l.SetFormat("[{{.Status}} {{.Duration}}] - {{.Request.UserAgent}}")
-
-	l := negronilogrus.NewMiddleware()
-	// l := negronilogrus.NewCustomMiddleware(logrus.DebugLevel, &logrus.JSONFormatter{}, "web")
-	n.Use(l)
-}
-
-func initRecoveryMiddleware(n *negroni.Negroni) {
-	r := negroni.NewRecovery()
-	n.Use(r)
+	app.Start()
 }
