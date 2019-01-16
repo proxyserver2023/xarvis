539b9a168215a9a820cf5946c96e2a8f7fa3821b
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-01-16 17:32:28 +0600

    gorsk testdata config

--- pkg/utl/config/config_test.go
+++ pkg/utl/config/config_test.go
@@ -18,6 +18,40 @@ func TestLoad(t *testing.T) {
 			wantData: nil,
 			wantErr: true,
 		},
+		{
+			name:    "Fail on wrong file format",
+			path:    "testdata/config.invalid.yaml",
+			wantErr: true,
+		},
+		{
+			name: "Success",
+			path: "testdata/config.testdata.yaml",
+			wantData: &Configuration{
+				DB: &Database{
+					PSN:        "postgres://postgres:postgres@postgres",
+					LogQueries: true,
+					Timeout:    20,
+				},
+				Server: &Server{
+					Port:         ":8080",
+					Debug:        true,
+					ReadTimeout:  15,
+					WriteTimeout: 20,
+				},
+				JWT: &JWT{
+					Secret:           "testing",
+					Duration:         10,
+					RefreshDuration:  10,
+					MaxRefresh:       144,
+					SigningAlgorithm: "HS384",
+				},
+				App: &Application{
+					MinPasswordStr: 3,
+					SwaggerUIPath:  "assets/swagger",
+				},
+			},
+		},
+
 	}
 
 
