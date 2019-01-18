0127f53b51eb12ef1ae30320654789b481ad8576
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-01-18 19:22:19 +0600

    refactor - app Run() - comment so that we understand
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -7,10 +7,12 @@ import (
 	"github.com/alamin-mahamud/xarvis/pkg/router"
 )
 
+// Run - bootstraps the whole app
 func Run() {
 	// config.Init()
 	router := router.Init()
 	http.Handle("/", router)
+	log.Println("Starting the Server")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
