a5844f0ed74833abfd3aee7546e26fa893c79ee6
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-01-18 16:19:23 +0600

    add - usecase
    1. just another abstraction for serving handlers

---
+++ pkg/usecase/iuser.go
@@ -0,0 +1,12 @@
+package usecase
+
+import "github.com/alamin-mahamud/xarvis/pkg/model"
+
+// IUser - represents the base Repository
+type IUser interface {
+	List() (*model.Users, error)
+	Create(*model.User) error
+	Get(string) (*model.User, error)
+	Update(string, *model.User) error
+	Delete(string) error
+}
---
+++ pkg/usecase/user.go
@@ -0,0 +1,58 @@
+package usecase
+
+import (
+	"log"
+
+	"github.com/alamin-mahamud/xarvis/pkg/model"
+	"github.com/alamin-mahamud/xarvis/pkg/repository"
+)
+
+// User - represents the base Repository
+type User struct {
+	r repository.IUser
+}
+
+// NewUser - returns the implementation of IUser
+func NewUser(r repository.IUser) IUser {
+	return &User{r}
+}
+
+// List - lists all users
+func (u *User) List() (*model.Users, error) {
+	users, err := u.r.List()
+	if err != nil {
+		log.Fatalf("error %v", err)
+	}
+	return users, nil
+}
+
+// Create -
+func (u *User) Create(m *model.User) error {
+	err := u.r.Create(m)
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
+
+}
+
+// Get -
+func (u *User) Get(id string) (user *model.User, err error) {
+	user, err = u.r.Get(id)
+	return user, err
+}
+
+// Update -
+func (u *User) Update(id string, m *model.User) (err error) {
+	err = u.r.Update(id, m)
+	return err
+}
+
+// Delete - user by id
+func (u *User) Delete(id string) (err error) {
+	err = u.r.Delete(id)
+	return err
+}
