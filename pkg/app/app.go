25d61aa5588d557f6f6201354d9f1a0cfba139ff
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-02-02 09:30:35 +0600

    :alembic: application specific changes

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,15 +1,27 @@
 package app
 
 import (
+	"context"
 	"log"
 	"net/http"
 
+	"github.com/alamin-mahamud/xarvis/pkg/database"
 	"github.com/alamin-mahamud/xarvis/pkg/router"
 )
 
 // Run - bootstraps the whole app
 func Run() {
 	// config.Init()
+
+	session, err := database.NewDB()
+	defer session.Close()
+
+	mPool := mgoSession.NewPool(context.Background(), session, 10)
+	defer mPool.Close()
+
+	br := bookmarkRepository.NewMongoRepository(mPool, config.MONGODB_DATABASE)
+	bu := bookmarkUsecase.NewService(br)
+
 	router := router.Init()
 	http.Handle("/", router)
 	log.Println("Starting the Server")
